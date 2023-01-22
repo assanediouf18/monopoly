0951330ef5a393a5676b89b231a348b1712f3f5c
egonin <emmagonin1@gmail.com>
2023-01-22 12:19:31 +0100

    fix arriverSur Compagnie

--- monopoly/Compagnie.cpp
+++ monopoly/Compagnie.cpp
@@ -8,28 +8,34 @@ Compagnie::Compagnie(string Nom, int p, int idCase):Propriete(Nom, p, idCase){
 
 
 void Compagnie::arriverSur(Joueur& j, Banque& bank) {
+	Propriete::arriverSur(j, bank);
 	int d1 = rand() % 6 + 1;
 	int d2 = rand() % 6 + 1; // lancer des deux dés
 	int loyer;
 	Joueur* joueur = &j;
 	Joueur Joueuractuel = *joueur;
-	if (joueur == Proprietaire) {
-		if (Joueuractuel == AutreCompagnie->getProprietaire()) {
-			loyer = (d1 + d2) * 10;
-			cout << "Vous etes debite de " << loyer << " euros, car vous etes proprietaire des deux compagnies !" << endl;
+	if (estachetee) {
+		if (joueur == Proprietaire) {
+			if (Joueuractuel == AutreCompagnie->getProprietaire()) {
+				loyer = (d1 + d2) * 10;
+				cout << "Vous etes debite de " << loyer << " euros, car vous etes proprietaire des deux compagnies !" << endl;
+			}
+			else {
+				loyer = (d1 + d2) * 4; ;
+				cout << "Vous etes debite de " << loyer << " euros" << endl;
+			}
 		}
 		else {
 			loyer = (d1 + d2) * 4; ;
 			cout << "Vous etes debite de " << loyer << " euros" << endl;
 		}
+		Joueuractuel.setSolde(Joueuractuel.getSolde() - loyer);
+		//bank.recevoir(loyer, Joueuractuel);
+		cout << "Vous avez un solde de " << Joueuractuel.getSolde() << " euros" << endl;
 	}
 	else {
-		loyer = (d1 + d2) * 4; ;
-		cout << "Vous etes debite de " << loyer << " euros" << endl;
+		acheter(&j, bank);
 	}
-	Joueuractuel.setSolde(Joueuractuel.getSolde() - loyer);
-	//bank.recevoir(loyer, Joueuractuel);
-	cout << "Vous avez un solde de " << Joueuractuel.getSolde() << " euros" << endl;
 
 
 }
