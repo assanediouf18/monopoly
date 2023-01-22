54084eed0e2e65c68c42309b99a7150395d6bb12
adiouf <h8799r@gmail.com>
2023-01-22 19:31:48 +0100

    Limite de l'argent que la banque peut céder au max

--- monopoly/banque.cpp
+++ monopoly/banque.cpp
@@ -4,6 +4,12 @@ Banque::Banque() : solde(10000) {
 }
 
 void Banque::payer(int montant, Joueur& joueur) {
+    if (montant > solde)
+    {
+        cout << "La banque n'as pas assez pour vous payer plus que " << solde << "..." << endl;
+        montant = solde;
+        cout << "Vous recevez donc " << solde << " M." << endl;
+    }
     solde -= montant;
     joueur.changeSolde(montant);
 }
